4a76acf5637ad4a74aa271d50a29bfa1709c3b55
Luigi Sousa <123770497+LuigiSousaa@users.noreply.github.com>
2024-02-02 12:12:11 -0300

    alteracoes

---
+++ Elevador/src/main/java/com/projeto/View/AmbienteGUI.java
@@ -0,0 +1,62 @@
+package com.projeto.View;
+
+import javax.swing.*;
+import java.awt.*;
+import java.net.URL;
+
+public class AmbienteGUI extends JFrame {
+    private JButton chamarButton;
+    private JLabel elevadorEsquerda;
+    private JLabel elevadorDireita;
+    private ImageIcon abertasIcon;
+    private ImageIcon fechadasIcon;
+    ImageIcon botaoIcon;
+
+    public AmbienteGUI() {
+        setTitle("Grid Layout Example");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Instanciando as classes para usar os ícones
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        URL abertasIconUrl = classLoader.getResource("elevadores_portas_abertas.png");
+        abertasIcon = new ImageIcon(abertasIconUrl);
+
+        URL fechadasIconUrl = classLoader.getResource("elevadores_portas_fechadas.png");
+        fechadasIcon = new ImageIcon(fechadasIconUrl);
+
+        URL botaoIconUrl = classLoader.getResource("botaoChamar.png");
+        botaoIcon = new ImageIcon(botaoIconUrl);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(9, 3));
+
+        // Iterando sobre as linhas e colunas
+        for (int i = 9; i >= 1; i--) {
+                String nomeArquivo = "Andar" + i + ".png";
+                ImageIcon andarIcon = new ImageIcon("resources/" + nomeArquivo);
+    
+                JLabel elevadorEsquerda = new JLabel(new ImageIcon(fechadasIcon.getImage().getScaledInstance(80, 60, Image.SCALE_SMOOTH)));
+                JButton chamarButton = new JButton(andarIcon);
+                chamarButton.setContentAreaFilled(false);
+                chamarButton.setBorderPainted(false);
+                chamarButton.setFocusPainted(false);
+                chamarButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+                JLabel elevadorDireita = new JLabel(new ImageIcon(fechadasIcon.getImage().getScaledInstance(80, 60, Image.SCALE_SMOOTH)));
+
+            panel.add(elevadorEsquerda);
+            panel.add(chamarButton);
+            panel.add(elevadorDireita);
+        }
+
+        add(panel);
+    }
+
+    public void run() {
+        pack();
+        setVisible(true);
+        setSize(400, 700);
+        setResizable(false);
+        setLocationRelativeTo(null);
+    }
+}
